080a1fd99785fe2099a216ba3779b7dfae93e1d2
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2021-01-06 22:35:23 +0200

    added hero section, full responsivness and slider

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import { swiper } from "./swiper.js";
+
+swiper();
---
+++ js/swiper.js
@@ -0,0 +1,45 @@
+export function swiper() {
+   const swiper = new Swiper(".hero__slider", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      grabCursor: true,
+      slideToClickedSlide: true,
+      preloadImages: false,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      loop: true,
+      lazy: {
+         loadPrevNext: true,
+         loadOnTransitionStart: true,
+      },
+      autoplay: {
+         delay: 3000,
+         disableOnInteraction: false
+      },
+      keyboard: {
+         enabled: true,
+         onlyInViewport: true,
+      },
+      breakpoints: {
+         620: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+         },
+         992: {
+            slidesPerView: 3,
+            spaceBetween: 35,
+         },
+         1200: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+         },
+
+         1600: {
+            slidesPerView: 5,
+            spaceBetween: 45,
+         },
+      },
+      observer: true,
+      observeParents: true,
+   });
+}
