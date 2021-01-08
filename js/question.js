ab59543722e0b44ccd57f82319d8228feceef515
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2021-01-08 18:50:10 +0200

    added custom quiz questions

---
+++ js/question.js
@@ -0,0 +1,58 @@
+const quizQuestions = [
+   {
+      question: "Sabaton is a band from Sweden, but from what city do they hail?",
+      answerOptions: ["Lund", "Gothenburg", "Falun", "Stockholm"],
+      correctAnswer: 2
+   },
+   {
+      question: "How many studio albums does Sabaton band have?",
+      answerOptions: [7, 9, 8, 10],
+      correctAnswer: 1
+   },
+   {
+      question: "Which song does describe a major World War II operation by the Polish Resistance home Army against Nazi Germany?",
+      answerOptions: ["Final Solution", "Art Of War", "40:1", "Uprising"],
+      correctAnswer: 3
+   },
+
+   {
+      question: "Which song does describe a brave 77th US Division which was surrounded by the Germans in Argonne?",
+      answerOptions: ["Winged Hussars", "82nd All The Way", "The Lost Batalion", "Last Dying Breath"],
+      correctAnswer: 2
+   },
+
+   {
+      question: "Which one of these music videos features a role by the Swedish actor Peter Stomare?",
+      answerOptions: ["Uprising", "Screaming Eagles", "Ghost Division", "Cliffs Of Gallipoli"],
+      correctAnswer: 0
+   },
+   
+   {
+      question: "What battle was centered around the song `Price Of A Mile` from the album `The Art of War`?",
+      answerOptions: ["The Battle of Berlin", "The Battle of Kursk", "The Battle of Warsaw", "The Battle of Passchendaele"],
+      correctAnswer: 3
+   },
+
+   {
+      question: "'Pride of a nation, a beast made of steel!' the words from the following song",
+      answerOptions: ["The Great War", "Bismarck", "Panzerkampf", "Ghost Division"],
+      correctAnswer: 1
+   },
+
+   {
+      question: "In which song an 'escape corridor' out of Berlin and a general of the 12th army who created it were glorified? ",
+      answerOptions: ["Attero Dominatus", "The Red Baron", "Hearts Of Iron", "Rise Of Evil"],
+      correctAnswer: 2
+   },
+   {
+      question: "Who was the deadliest and the most dangerous sniper during WWI? ",
+      answerOptions: ["Francis Pegahmagabow", "Noah Adamia", "Hiram Berdan", "Herman Davis"],
+      correctAnswer: 0
+   },
+   
+   {
+      question: "Who did lead 16 men against the German defensive machine gun during the Meuse-Argonne Offensive",
+      answerOptions: ["Frank Luke", "Noah Adamia", "Hiram Berdan", "Alvin York"],
+      correctAnswer: 3
+   },
+]
